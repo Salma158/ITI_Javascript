a7cec18cb7bb10eb736fb4fb39205841bbc1f2b8
Salma Sherif <Salmasherif856@gmail.com>
2023-12-17 00:12:00 +0200

    Add files via upload

---
+++ 2.task 1/task 1.js
@@ -0,0 +1,25 @@
+console.log("q1")
+for(var x = -10; x <= 19; x++){
+    console.log(x)
+}
+
+console.log("q2")
+for(var x=10; x<=40; x++){
+    if(x % 2 == 0){
+    console.log(x)
+    } 
+}
+
+console.log("q3")
+for(var x=300; x<=333; x++){
+    if(x % 2 != 0){
+    console.log(x)
+    } 
+}
+
+console.log("q4")
+for(var x=5; x<=55; x++){
+    if(x % 3 == 0 && x % 5 ==0){
+    console.log(x)
+    } 
+}
