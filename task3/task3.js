1f23ade2b471579f7cb7fbedbee21e3de8696a0f
Salma Sherif <Salmasherif856@gmail.com>
2023-12-15 23:26:56 -0800

    Delete task3 directory

--- task3/task3.js
+++
@@ -1,9 +0,0 @@
-var x = prompt("make a guess")
-
-if ( x > 7 ){
-    console.log("the number is too high")
-} else if ( x < 7){
-    console.log("the number is too small")
-} else{
-    console.log("true :)")
-}
